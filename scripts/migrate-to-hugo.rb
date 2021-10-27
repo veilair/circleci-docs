c5c5cb39fca724c6caca8fa888f594e69cae9f71
teesloane <tylersloane@gmail.com>
2021-10-27 10:47:39 -0400

    Add: migrate asciidoc + frontmatter.

--- scripts/migrate-to-hugo.rb
+++ scripts/migrate-to-hugo.rb
@@ -8,7 +8,7 @@ class Migrate
                    }
 
   @@dirs_to_delete = ["hugo/static/assets/img/docs/_unused"]
-  @@replacements_to_make = {
+  @@md_syntax_mapping = {
     /\{\{\s*site.baseurl\s*\}\}/ => '{{< baseurl >}}',
     "endraw"                     => '/raw',
     /\{:\s*.no_toc/              => "{.no_toc}",
@@ -22,14 +22,12 @@ class Migrate
 
   def initialize
     copy_2_0
-    replace_syntax
+    replace_md_syntax
+    migrate_asciidoc
     rename_index
     print_manual_work
   end
 
-  def get_markdown_files(dir)
-    return Dir.glob("#{dir}/**/*.md")
-  end
 
   # jekyll has an index.md, which hugo expects to be _index to differentiate it
   # as a "list" page type, rather than a" single
@@ -37,41 +35,69 @@ class Migrate
     File.rename("hugo/content/2.0/index.md", "hugo/content/2.0/_index.md") if File.file?("hugo/content/2.0/index.md")
   end
 
-  # iterate on all markdown files,
-  # open them, and for each jekyll syntax type, change it to hugo's equivalent.
-  def replace_syntax
-    ## for each dir that moved to hugo...
+  # glob all markdown files that were copied into hugo, open them, swap
+  # instances of jekyll syntax for hugo.
+  def replace_md_syntax
     @@dirs_to_copy.each do | _, hugo|
-      ## glob all md files and
-      files = get_markdown_files(hugo)
-      ## open each file, and for each replacement, make the replacement.
+      files =  Dir.glob("#{hugo}/**/*.md")
       files.each do |f|
-        # p f
         file_text = File.read(f)
-        # replaced_text = ""
-        @@replacements_to_make.each do |jekyll_syntax, hugo_syntax|
-          # replaced_text = file_text.gsub!(jekyll_syntax, hugo_syntax)
+        @@md_syntax_mapping.each do |jekyll_syntax, hugo_syntax|
           file_text.gsub!(jekyll_syntax, hugo_syntax)
-
         end
-
         File.open(f, "w") { |file| file.puts file_text }
       end
     end
   end
 
+  def has_frontmatter?(file_lines, f)
+    if file_lines[0] != "---"
+      return false
+    end
 
-  # our ascidoc files are unique:
-  # First - they don't have a title: front matter - jekyll renders the first `=`
-  # as the title.
-  #
-  # Next - if it DID have a title front matter - it gets shoved into the PDF we
-  # generate with asciidoctor-pdf
-  #
-  # SO - to convert for hugo we have to:
-  # a) loop over every file and grab the first h1 level heading (=) and turn it into front matter
-  # b) to make sure the pdfs render, we disable front matter in the scripts/buid_pdfs.sh script.
-  def migrate_ascidoc
+    # make sure there is a matching output ---
+    file_lines.drop(1).each do |line|
+      if line == "---"
+        return true
+      end
+    end
+    raise "No closing front matter for file: #{f}"
+  end
+
+  # assumes you already have a file with frontmatter,
+  def insert_into_frontmatter(item, file_lines, f)
+    first_line = file_lines[0]
+    return [first_line, item, file_lines[1..-1]]
+  end
+
+  # loop through lines and get first instance of top level title.
+  def get_asciidoc_title(file_lines, f)
+    title_regex = /^=[^=]/
+    file_lines.each do |line|
+      if line.match(title_regex)
+        return line.split(title_regex)[1]
+      end
+    end
+  end
+
+
+  # The way we format asciidoc for jekyll is different than what hugo needs.
+  # This method extracts the title syntax and turns it into front matter on our adoc files.
+  def migrate_asciidoc
+    @@dirs_to_copy.each do | _, hugo|
+      adoc_files = Dir.glob("#{hugo}/**/*.adoc")
+      adoc_files.each do |f|
+        file_lines = File.readlines(f, chomp: true)
+        title = get_asciidoc_title(file_lines, f)
+        if has_frontmatter?(file_lines, f)
+          output = insert_into_frontmatter("title: #{title}", file_lines, f)
+          File.open(f, "w") { |file| file.puts output }
+        else
+          output = ["---\n", "title: \"#{title}\"", "---\n"] + file_lines
+          File.open(f, "w") { |file| file.puts output }
+        end
+      end
+    end
   end
 
   def print_manual_work
